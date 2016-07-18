f6f7f4555cbb877d4a51206e50856731b6f5e510
Stian Eklund <stian.eklund@gmail.com>
2016-07-18 19:14:36 +0200

    cleanup and add more documentation

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
-/// Rosten track Bring packages in Rust
+/// Rosten
+/// Track Bring packages in Rust
 /// For testing use "TESTPACKAGE-AT-PICKUPPOINT" as tracking number
 /// https://www.mybring.com/tracking/api
-
-/// Request parameters
-/// Query param     Type        Description
-/// q               string      Reference, package number, shipment num to query
-/// callback        string      Optional JSONP callback function name
-///
-/// Endpoints for Base URL
-///
-/// Method      Endpoint                        Usage
-/// GET         /tracking{mediaTypeExtension}   Get information about a shipment
-///
-/// Request string for JSON would look like tracking.bring.com/tracking.json?q=trackingnumber
 
 extern crate curl;
 extern crate clap;
@@ -27,8 +16,10 @@ use curl::easy::Easy;
 use std::fmt::Display;
 use serde_json::Value;
 
-// TODO Define contents of JSON. Would an enum be better here?
-// Perhaps put structs in different file?
+// TODO Define values corresponding to json file
+// Would an enum be better here?
+
+// TODO Depending on size, perhaps move structs to different file?
 
 struct Data {
     Firstname: String,
@@ -38,6 +29,7 @@ struct Data {
     PhoneNumber: Vec<String>
 }
 // Do we need to define everything in tracking.json?
+// Is it enough to assign whatever matches to needed values?
 struct Address {
     Street: String,
     City: String,
@@ -46,6 +38,7 @@ struct Address {
 
 pub fn main() {
 
+    // TODO Figure out ~/ path so compiler can find the file easily
     let path = Path::new("/home/stian/projects/rosten/tracking.json");
     let display = path.display();
     let mut file = match File::open(&path) {
@@ -55,14 +48,16 @@ pub fn main() {
 
     let mut buffer = String::new();
     file.read_to_string(&mut buffer).unwrap();
+
     // TODO Better names
     let data: Value = serde_json::from_str(&buffer).unwrap();
-    // TODO Iterate over consignmentSet[array]? How?
+
+    // TODO Iterate over consignmentSet
     let values = data.as_object()
         .and_then(|object| object.get("consignmentSet"))
         .and_then(|values| values.as_object())
         .unwrap_or_else(|| {
-            panic!("Failed to get object value from json"); // currently fails here
+            panic!("Failed to get object value from json"); // fails at runtime here
         });
 
     for (key, value) in values.iter() {
@@ -74,77 +69,3 @@ pub fn main() {
         println!("{} -> {:?}", key, results);
     }
 }
-
-
-
-// TODO Handle external GET
-//    let mut easy = Easy::new();
-//    easy.url(&json_file).unwrap();
-//    easy.get("TESTPACKAGE-AT-PICKUPPOINT");
-
-// get_http_body from url
-//    let http_body = std::str::from_utf8(http_response.get_http_body()).unwrap_or_else(|err| {
-//    panic!("Failed to get response {}; error is {}", url, err); // TODO Error handling
-//    });
-
-
-
-
-
-
-
-
-// import io, os, filecmp // assert_eq!()
-// import json, requests
-// import argparse
-
-// parser = argparse.ArgumentParser()
-// parser.add_argument("sid")
-// args = parser.parse_args()
-
-// sid = args.sid // tracking ID
-
-// params = dict(
-//             q=sid
-//         )
-//
-// resp = requests.get(url=url, params=params)
-// data = json.loads(resp.text)
-//
-// path = '/home/ix/src/posten/data'
-// first = True
-// f_i = 0
-// f_new = f_old = path + '/' + 'posten-{0}-{1}'.format(sid, f_i) + '.json'
-// if os.path.exists(f_new):
-//     first = False
-//     while os.path.exists(f_new):
-//             f_old = path + '/' + 'posten-{0}-{1}'.format(sid, f_i) + '.json'
-//             f_i += 1
-//             f_new = path + '/' + 'posten-{0}-{1}'.format(sid, f_i) + '.json'
-// #print 'f_old: ' + f_old
-// #print 'f_new: ' + f_new
-//
-// # print status for i3
-// if not os.path.exists('/tmp/posten'):
-//     os.mkdir('/tmp/posten')
-//
-// e_i = 0
-// events = []
-// for i in range(0, len(data['consignmentSet'][0]['packageSet'][0]['eventSet'])):
-//     events.append(data['consignmentSet'][0]['packageSet'][0]['eventSet'][i]['status'])
-// e_i = 0
-// while events[e_i] == u'NOTIFICATION_SENT':
-//     e_i = e_i + 1
-//
-// with open('/tmp/posten/' + sid, 'w') as status_file:
-//     status_file.write(events[e_i])
-//
-// with open(f_new, 'w') as outfile:
-//      json.dump(data, outfile, sort_keys = True, indent = 4, ensure_ascii=False)
-//
-// if not first:
-//     if filecmp.cmp(f_old, f_new):
-//         print 'no change'
-//         os.remove(f_new)
-//     else:
-//         print 'new entry'
