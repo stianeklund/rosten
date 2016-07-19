f4a495f6293b4cb225c2531b21bc6ab839200e2b
Stian Eklund <stian.eklund@gmail.com>
2016-07-19 08:42:48 +0200

    try to wrap structs in mod

--- src/response.rs
+++ src/response.rs
@@ -1,72 +1,74 @@
-pub struct JsonConsignmentsetPackagesetSenderaddress {
-    pub addressLine1: String,
-    pub addressLine2: String,
-    pub postalCode: String,
-    pub city: String,
-    pub countryCode: String,
-    pub country: String,
-}
+mod {
+    pub struct JsonConsignmentsetPackagesetSenderaddress {
+        pub addressLine1: String,
+        pub addressLine2: String,
+        pub postalCode: String,
+        pub city: String,
+        pub countryCode: String,
+        pub country: String,
+    }
 
-pub struct JsonConsignmentsetPackagesetEventsetRecipientsignature {
-    pub name: String,
-}
+        pub struct JsonConsignmentsetPackagesetEventsetRecipientsignature {
+            pub name: String,
+        }
 
-pub struct JsonConsignmentsetPackagesetEventsetDefinition {
-    pub term: String,
-    pub explanation: String,
-}
+        pub struct JsonConsignmentsetPackagesetEventsetDefinition {
+            pub term: String,
+            pub explanation: String,
+        }
 
-pub struct JsonConsignmentsetPackagesetEventset {
-    pub description: String,
-    pub status: String,
-    pub recipientSignature: JsonConsignmentsetPackagesetEventsetRecipientsignature,
-    pub unitId: String,
-    pub unitInformationUrl: String,
-    pub unitType: String,
-    pub postalCode: String,
-    pub city: String,
-    pub countryCode: String,
-    pub country: String,
-    pub dateIso: String,
-    pub displayDate: String,
-    pub displayTime: String,
-    pub consignmentEvent: bool,
-    pub definitions: Vec<JsonConsignmentsetPackagesetEventsetDefinition>,
-}
+        pub struct JsonConsignmentsetPackagesetEventset {
+            pub description: String,
+            pub status: String,
+            pub recipientSignature: JsonConsignmentsetPackagesetEventsetRecipientsignature,
+            pub unitId: String,
+            pub unitInformationUrl: String,
+            pub unitType: String,
+            pub postalCode: String,
+            pub city: String,
+            pub countryCode: String,
+            pub country: String,
+            pub dateIso: String,
+            pub displayDate: String,
+            pub displayTime: String,
+            pub consignmentEvent: bool,
+            pub definitions: Vec<JsonConsignmentsetPackagesetEventsetDefinition>,
+        }
 
-pub struct JsonConsignmentsetPackageset {
-    pub statusDescription: String,
-    pub descriptions: Vec,
-    pub packageNumber: String,
-    pub previousPackageNumber: String,
-    pub productName: String,
-    pub productCode: String,
-    pub brand: String,
-    pub lengthInCm: i64,
-    pub widthInCm: i64,
-    pub heightInCm: i64,
-    pub volumeInDm3: f64,
-    pub weightInKgs: f64,
-    pub pickupCode: String,
-    pub dateOfReturn: String,
-    pub senderName: String,
-    pub senderAddress: JsonConsignmentsetPackagesetSenderaddress,
-    pub recipientHandlingAddress: JsonConsignmentsetPackagesetSenderaddress,
-    pub eventSet: Vec<JsonConsignmentsetPackagesetEventset>,
-}
+        pub struct JsonConsignmentsetPackageset {
+            pub statusDescription: String,
+            pub descriptions: Vec,
+            pub packageNumber: String,
+            pub previousPackageNumber: String,
+            pub productName: String,
+            pub productCode: String,
+            pub brand: String,
+            pub lengthInCm: i64,
+            pub widthInCm: i64,
+            pub heightInCm: i64,
+            pub volumeInDm3: f64,
+            pub weightInKgs: f64,
+            pub pickupCode: String,
+            pub dateOfReturn: String,
+            pub senderName: String,
+            pub senderAddress: JsonConsignmentsetPackagesetSenderaddress,
+            pub recipientHandlingAddress: JsonConsignmentsetPackagesetSenderaddress,
+            pub eventSet: Vec<JsonConsignmentsetPackagesetEventset>,
+        }
 
-pub struct JsonConsignmentset {
-    pub consignmentId: String,
-    pub previousConsignmentId: String,
-    pub packageSet: Vec<JsonConsignmentsetPackageset>,
-    pub senderName: String,
-    pub senderAddress: JsonConsignmentsetPackagesetSenderaddress,
-    pub recipientHandlingAddress: JsonConsignmentsetPackagesetSenderaddress,
-    pub senderReference: String,
-    pub totalWeightInKgs: f64,
-    pub totalVolumeInDm3: f64,
-}
+        pub struct JsonConsignmentset {
+            pub consignmentId: String,
+            pub previousConsignmentId: String,
+            pub packageSet: Vec<JsonConsignmentsetPackageset>,
+            pub senderName: String,
+            pub senderAddress: JsonConsignmentsetPackagesetSenderaddress,
+            pub recipientHandlingAddress: JsonConsignmentsetPackagesetSenderaddress,
+            pub senderReference: String,
+            pub totalWeightInKgs: f64,
+            pub totalVolumeInDm3: f64,
+        }
 
-pub struct Json {
-    pub consignmentSet: Vec<JsonConsignmentset>,
+        pub struct Json {
+            pub consignmentSet: Vec<JsonConsignmentset>,
+        }
 }
