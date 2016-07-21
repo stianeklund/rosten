14a2384394617e6204012873204f75e1a146e41c
Stian Eklund <stian.eklund@gmail.com>
2016-07-21 16:28:03 +0200

    print out package event status

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,15 @@ use hyper::{Client};
 use json_response::BringResponse;
 
 pub fn main() {
+    // TODO Get tracking numbers from external file?
+    // Crontab process to poll every hour?
 
-    // Read user input
-    println!("Enter tracking number:");
     let mut input = String::new();
+    println!("Enter tracking number:");
     io::stdin().read_line(&mut input);
+    // let input = "TESTPACKAGE-AT-PICKUPPOINT";
 
-    // TODO Implement http query, check hyper docu
+    // TODO Look up Hyper query. Difference between this solution?
     let url = format!("https://tracking.bring.com/tracking.json?q={}", input);
     let url = url.as_str(); // convert &str into a String
 
@@ -32,19 +34,24 @@ pub fn main() {
         try!(response.read_to_string(&mut buf));
         Ok(buf)
     }
-
+    // TODO Perhaps wrap deserializing into own function?
     let buf = get_content(url).unwrap();
     // Here we pass & coerce &String (result of get_content) to json::decode
-    let deserialize :BringResponse = json::decode(&buf).unwrap();
+    let deserialize: BringResponse = json::decode(&buf).unwrap();
     let sets = deserialize.consignmentSet;
-
+    // TODO Improve on for loop or look into alternative ways to do this.
     for i in 0..sets.len() {
-        let set = &sets[i];
-        println!("Sets is {}", set.senderName);
-        for x in 0..set.packageSet.len() {
-            let package_set = &set.packageSet[x];
-            println!("Packageset number is {}", x);
-            println!("Status is: {}", package_set.statusDescription);
+        let consignment_set = &sets[i];
+        for x in 0..consignment_set.packageSet.len() {
+            let package_set = &consignment_set.packageSet[x];
+            println!("Package number: {}", package_set.packageNumber);
+            println!("Sender name: {:?}", package_set.senderName);
+            println!("Package Status: {}", package_set.statusDescription);
+            for n in 0..package_set.eventSet.len() {
+                let event_set = &package_set.eventSet[n];
+                println!("Event status: {}", event_set.status);
+                println!("Event description: {}", event_set.description);
+            }
         }
     }
 }
