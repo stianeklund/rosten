a9c9fe35c159a59cc646ef7a288dc0be69f7c1e0
Stian Eklund <stian.eklund@gmail.com>
2016-07-25 13:51:38 +0200

    add break to end iteration early. we don't need to know the entire package history, write that into separate function

--- src/main.rs
+++ src/main.rs
@@ -29,9 +29,6 @@ pub fn main() {
         .get_matches();
 
     let input = matches.value_of("track").unwrap();
-    // let input = "TESTPACKAGE-AT-PICKUPPOINT";
-
-    // TODO Look up Hyper query. Difference between this solution?
     let url = format!("https://tracking.bring.com/tracking.json?q={}", input);
     let url = url.as_str(); // convert &str into a String
 
@@ -42,27 +39,27 @@ pub fn main() {
         try!(response.read_to_string(&mut buf));
         Ok(buf)
     }
+
     let buf = get_content(url).unwrap();
 
     fn deserialize(buf: &str) {
         // pass & coerce &String (result of get_content) to json::decode
         let deserialize: BringResponse = json::decode(&buf).unwrap();
         let sets = deserialize.consignmentSet;
-        // TODO Improve on for loop or look into alternative ways to do this.
         for i in 0..sets.len() {
             let consignment_set = &sets[i];
             for x in 0..consignment_set.packageSet.len() {
                 let package_set = &consignment_set.packageSet[x];
                 println!("Package number: {}", package_set.packageNumber);
                 println!("Sender name: {:?}", package_set.senderName);
-                println!("Package Status: {}", package_set.statusDescription);
                 for n in 0..package_set.eventSet.len() {
                     let event_set = &package_set.eventSet[n];
                     println!("Event status: {}", event_set.status);
                     println!("Event description: {}", event_set.description);
-                }
-            }
-        }
+                    break;
+                 }
+             }
+         }
     }
     deserialize(&buf);
 }
